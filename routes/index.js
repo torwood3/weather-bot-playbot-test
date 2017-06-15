375a58561476c531f6156013bf45804e086e98d6
pamasse <pa.masse@outlook.fr>
2017-06-15 15:26:45 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -43,6 +43,7 @@ router.post('/weather', function(req, res, next) {
     }
     request(options, function (error, response, body) {
         if (!error && response.statusCode == 200) {
+            body = JSON.parse(body);
             console.log(body["cnt"]);
             if (body.cnt >= diff) {
                 const result = body.list[diff];
